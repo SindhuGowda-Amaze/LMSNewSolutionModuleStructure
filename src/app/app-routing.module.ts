d66d9662d607e0674422d36a93cf8152c760612b
Divya <divyashree@amazeinc.in>
2022-06-28 16:15:01 +0530

    dddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./Modules/admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  { path: 'Admin', loadChildren: () => import('./Modules/admin/admin.module').then(m => m.AdminModule) }, 
+  { path: 'Employee', loadChildren: () => import('./Modules/employee/employee.module').then(m => m.EmployeeModule) }, 
+  { path: 'HR', loadChildren: () => import('./Modules/hr/hr.module').then(m => m.HRModule) }, 
+  { path: 'Manager', loadChildren: () => import('./Modules/manager/manager.module').then(m => m.ManagerModule) }, 
+  { path: 'Shared', loadChildren: () => import('./Modules/shared/shared.module').then(m => m.SharedModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
