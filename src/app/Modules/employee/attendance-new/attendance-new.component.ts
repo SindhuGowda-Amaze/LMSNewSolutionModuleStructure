9e7abbcc466c4e2d22d7ea0faf2f27c77543b0cc
Divya <divyashree@amazeinc.in>
2022-08-17 18:38:47 +0530

    dddd

--- src/app/Modules/employee/attendance-new/attendance-new.component.ts
+++ src/app/Modules/employee/attendance-new/attendance-new.component.ts
@@ -80,35 +80,6 @@ this.getTopic();
   }
 
 
-// areYouReallySure:any;
- 
-// public  areYouSure() {
-//     if(this.allowPrompt){
-//         if (!this.areYouReallySure && true) {
-//             this.areYouReallySure = true;
-//             var confMessage = "***************************************\n\n W A I T !!! \n\nBefore leaving our site, follow CodexWorld for getting regular updates on Programming and Web Development.\n\n\nCLICK THE *CANCEL* BUTTON RIGHT NOW\n\n***************************************";return confMessage;
-//         }
-//     }else{
-//         this.allowPrompt = true;
-//     }
-// }
-
-// allowPrompt = true;
-// window.onbeforeunload = this.areYouSure;
-
-
-  // public GetEnroll(){
-  //   this.LearningService.GetEnroll().subscribe(
-  //     data => {
-  //       debugger
-  //       // this.result = data.filter(x => x.manager == this.manager );
-  //       // this.result = data.filter(x => x.status == 'Manager Assigned' );
-  //       this.result =  data.filter(x => x.type == 'Manager Assign')
-  //       this.count = this.result.length;
-  //     })
-  // }
-
-
   endDate: any;
   uniquelist: any;
   Date: any;
